7489b29c816d818dd2e24db087b010465c366eb0
Samir70 <sbetmouni@talktalk.net>
2022-11-07 12:33:49 +0100

    progress integration test

--- MusicLibrary/spec/integrate_spec.rb
+++ MusicLibrary/spec/integrate_spec.rb
@@ -2,7 +2,11 @@ require 'music_library'
 require 'track'
 
 RSpec.describe "integration" do
-    xit "searches for a key word" do
-        
+    xit "returns a list of tracks that match a keyword" do
+        ml = MusicLibrary.new
+        t1 = Track.new("Money", "Pink Floyd")
+        t2 = Track.new("Hysteria", "Muse")
+        t3 = Track.new("Pink Vinci", "Mona Lisa")
+        expect(ml.search("Mon")).to eq [t1, t3]
     end
 end
